9325feee004ec69c6e8ed2c0664555cb63893f01
aagam94 <aagam94@gmail.com>
2014-01-04 17:04:49 +0530

    sample layout

--- gen/glass/remindme/R.java
+++ gen/glass/remindme/R.java
@@ -10,6 +10,12 @@ package glass.remindme;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int black=0x7f040000;
+        public static final int gray=0x7f040002;
+        public static final int red=0x7f040003;
+        public static final int white=0x7f040001;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -17,37 +23,62 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int ic_colon=0x7f020000;
+        public static final int ic_lap=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int ic_pause=0x7f020003;
+        public static final int ic_play=0x7f020004;
+        public static final int ic_reset=0x7f020005;
+        public static final int ic_stop=0x7f020006;
+        public static final int ic_timer=0x7f020007;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f090005;
+        public static final int change_timer=0x7f09000d;
+        public static final int hours=0x7f090001;
+        public static final int minutes=0x7f090002;
+        public static final int no_time_set=0x7f090006;
+        public static final int pause=0x7f09000b;
+        public static final int reset=0x7f09000c;
+        public static final int resume=0x7f09000a;
+        public static final int seconds=0x7f090003;
+        public static final int set_timer=0x7f090007;
+        public static final int start=0x7f090009;
+        public static final int stop=0x7f09000e;
+        public static final int time_set=0x7f090008;
+        public static final int tip=0x7f090004;
+        public static final int value=0x7f090000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int card_select_value=0x7f030001;
+        public static final int card_timer=0x7f030002;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
+        public static final int timer=0x7f080001;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int app_name=0x7f060000;
+        public static final int change_time=0x7f060003;
+        public static final int hours=0x7f06000a;
+        public static final int minutes=0x7f06000b;
+        public static final int pause=0x7f060004;
+        public static final int reset=0x7f060005;
+        public static final int resume=0x7f060006;
+        public static final int seconds=0x7f06000c;
+        public static final int set_timer=0x7f060007;
+        public static final int start=0x7f060008;
+        public static final int start_timer=0x7f060001;
+        public static final int stop=0x7f060009;
+        public static final int timer_finished=0x7f060002;
     }
     public static final class style {
         /** 
-        Base application theme, dependent on API level. This theme is replaced
-        by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
-    
-
-            Theme customizations available in newer API levels can go in
-            res/values-vXX/styles.xml, while customizations related to
-            backward-compatibility can go here.
-        
-
         Base application theme for API 11+. This theme completely replaces
         AppBaseTheme from res/values/styles.xml on API 11+ devices.
     
@@ -59,10 +90,9 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
-        /**  Application theme. 
- All customizations that are NOT specific to a particular API-level can go here. 
-         */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppBaseTheme=0x7f070003;
+        public static final int ComponentIcon=0x7f070001;
+        public static final int MenuTheme=0x7f070002;
+        public static final int TimeComponent=0x7f070000;
     }
 }
--- src/glass/remindme/MainActivity.java
+++ src/glass/remindme/MainActivity.java
@@ -9,13 +9,13 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.card_timer);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
+		// Inflate the menu; t//his adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.timer, menu);
 		return true;
 	}
 
