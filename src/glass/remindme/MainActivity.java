bbd83d10db2d66c9fbc95577f8ff5bf51c903011
aagam94 <aagam94@gmail.com>
2014-01-04 18:04:39 +0530

    fetch image

--- src/glass/remindme/MainActivity.java
+++ src/glass/remindme/MainActivity.java
@@ -1,8 +1,12 @@
 package glass.remindme;
 
-import android.os.Bundle;
+import java.util.List;
+
 import android.app.Activity;
-import android.view.Menu;
+import android.content.Intent;
+import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -10,13 +14,28 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.card_timer);
+		displaySpeechRecognizer();
+	}
+	
+	private void displaySpeechRecognizer() {
+	    Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+	    startActivityForResult(intent, 132);
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; t//his adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.timer, menu);
-		return true;
+	protected void onActivityResult(int requestCode, int resultCode,
+	        Intent data) {
+	    if (requestCode == 132 && resultCode == RESULT_OK) {
+	        List<String> results = data.getStringArrayListExtra(
+	                RecognizerIntent.EXTRA_RESULTS);
+	        String spokenText = results.get(0);
+	        TextView tv1 = (TextView)findViewById(R.id.fetchtext);
+	        tv1.setText(spokenText);
+	        // Do something with spokenText.
+	    }
+	    super.onActivityResult(requestCode, resultCode, data);
 	}
+
+	
 
 }
