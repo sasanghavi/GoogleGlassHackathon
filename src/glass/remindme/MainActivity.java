4a7fee4d8097102dd1cb8c3e1e5ebdb23ec27774
Nihar Trivedi <nihar.trivedi888@gmail.com>
2014-01-05 00:32:08 +0530

    added check for number

--- src/glass/remindme/MainActivity.java
+++ src/glass/remindme/MainActivity.java
@@ -1,15 +1,20 @@
 package glass.remindme;
 
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.CalendarContract.Reminders;
 import android.speech.RecognizerIntent;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
+	public String data, time;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -35,7 +40,23 @@ public class MainActivity extends Activity {
 	    }
 	    super.onActivityResult(requestCode, resultCode, data);
 	}
-
+	public void processString(String input){
+		String temp=input;
+		if(temp.substring(0,9).equalsIgnoreCase("Remind me")){
+			input = input.substring(9,input.length());
+		}
+		
+		if(input.matches(".*\\d.*")){
+			ArrayList<String> inArrayList=new ArrayList<String>(Arrays.asList(input.split(" ")));
+			if(inArrayList.get(0).equalsIgnoreCase("to")||inArrayList.get(0).equalsIgnoreCase("about")){
+//				inArrayList.lastIndexOf(object)
+//				inArrayList.indexOf("")
+			}
+		}else{
+			System.out.println("You did not specified time");
+		}
+		
+	}
 	
 
 }
