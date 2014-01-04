e907dd41a3a3946a4027de4335e8407b53d9eda5
aagam94 <aagam94@gmail.com>
2014-01-04 17:42:41 +0530

    added voice trigger

--- gen/glass/remindme/R.java
+++ gen/glass/remindme/R.java
@@ -11,10 +11,10 @@ public final class R {
     public static final class attr {
     }
     public static final class color {
-        public static final int black=0x7f040000;
-        public static final int gray=0x7f040002;
-        public static final int red=0x7f040003;
-        public static final int white=0x7f040001;
+        public static final int black=0x7f050000;
+        public static final int gray=0x7f050002;
+        public static final int red=0x7f050003;
+        public static final int white=0x7f050001;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
@@ -23,8 +23,8 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f050000;
-        public static final int activity_vertical_margin=0x7f050001;
+        public static final int activity_horizontal_margin=0x7f060000;
+        public static final int activity_vertical_margin=0x7f060001;
     }
     public static final class drawable {
         public static final int ic_colon=0x7f020000;
@@ -37,21 +37,21 @@ public final class R {
         public static final int ic_timer=0x7f020007;
     }
     public static final class id {
-        public static final int action_settings=0x7f090005;
-        public static final int change_timer=0x7f09000d;
-        public static final int hours=0x7f090001;
-        public static final int minutes=0x7f090002;
-        public static final int no_time_set=0x7f090006;
-        public static final int pause=0x7f09000b;
-        public static final int reset=0x7f09000c;
-        public static final int resume=0x7f09000a;
-        public static final int seconds=0x7f090003;
-        public static final int set_timer=0x7f090007;
-        public static final int start=0x7f090009;
-        public static final int stop=0x7f09000e;
-        public static final int time_set=0x7f090008;
-        public static final int tip=0x7f090004;
-        public static final int value=0x7f090000;
+        public static final int action_settings=0x7f0a0005;
+        public static final int change_timer=0x7f0a000d;
+        public static final int hours=0x7f0a0001;
+        public static final int minutes=0x7f0a0002;
+        public static final int no_time_set=0x7f0a0006;
+        public static final int pause=0x7f0a000b;
+        public static final int reset=0x7f0a000c;
+        public static final int resume=0x7f0a000a;
+        public static final int seconds=0x7f0a0003;
+        public static final int set_timer=0x7f0a0007;
+        public static final int start=0x7f0a0009;
+        public static final int stop=0x7f0a000e;
+        public static final int time_set=0x7f0a0008;
+        public static final int tip=0x7f0a0004;
+        public static final int value=0x7f0a0000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -59,23 +59,23 @@ public final class R {
         public static final int card_timer=0x7f030002;
     }
     public static final class menu {
-        public static final int main=0x7f080000;
-        public static final int timer=0x7f080001;
+        public static final int main=0x7f090000;
+        public static final int timer=0x7f090001;
     }
     public static final class string {
-        public static final int app_name=0x7f060000;
-        public static final int change_time=0x7f060003;
-        public static final int hours=0x7f06000a;
-        public static final int minutes=0x7f06000b;
-        public static final int pause=0x7f060004;
-        public static final int reset=0x7f060005;
-        public static final int resume=0x7f060006;
-        public static final int seconds=0x7f06000c;
-        public static final int set_timer=0x7f060007;
-        public static final int start=0x7f060008;
-        public static final int start_timer=0x7f060001;
-        public static final int stop=0x7f060009;
-        public static final int timer_finished=0x7f060002;
+        public static final int app_name=0x7f070000;
+        public static final int change_time=0x7f070003;
+        public static final int hours=0x7f07000a;
+        public static final int minutes=0x7f07000b;
+        public static final int pause=0x7f070004;
+        public static final int reset=0x7f070005;
+        public static final int resume=0x7f070006;
+        public static final int seconds=0x7f07000c;
+        public static final int set_timer=0x7f070007;
+        public static final int start=0x7f070008;
+        public static final int start_timer=0x7f070001;
+        public static final int stop=0x7f070009;
+        public static final int timer_finished=0x7f070002;
     }
     public static final class style {
         /** 
@@ -90,9 +90,12 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f070003;
-        public static final int ComponentIcon=0x7f070001;
-        public static final int MenuTheme=0x7f070002;
-        public static final int TimeComponent=0x7f070000;
+        public static final int AppBaseTheme=0x7f080003;
+        public static final int ComponentIcon=0x7f080001;
+        public static final int MenuTheme=0x7f080002;
+        public static final int TimeComponent=0x7f080000;
+    }
+    public static final class xml {
+        public static final int voice_trigger_start=0x7f040000;
     }
 }
