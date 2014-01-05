a4ede7461049667f7f5b245c22ce7246f350fbec
aagam94 <aagam94@gmail.com>
2014-01-05 15:19:19 +0530

    timeline

---
+++ src/glass/remindme/Config.java
@@ -0,0 +1,13 @@
+package glass.remindme;
+
+
+public final class Config
+{
+    private Config() {}
+
+    // Log level.
+    public static int LOGLEVEL = android.util.Log.VERBOSE;
+    //static final int LOGLEVEL = android.util.Log.DEBUG;
+    //static final int LOGLEVEL = android.util.Log.INFO;
+
+}
