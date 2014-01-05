cbf3c1b75b7f6059ac1305275451e1d97827ba9b
vijay13 <vijay.atwork13@gmail.com>
2014-01-05 14:42:56 +0530

    Fixed bug in getReminderText

--- src/glass/remindme/MainActivity.java
+++ src/glass/remindme/MainActivity.java
@@ -185,8 +185,8 @@ public class MainActivity extends Activity {
 		}else if(text.indexOf("about") == 0){
 			return text.substring(6);
 		}else{
-			if(input.length() < this.lastIndexOfIndicator(input) && this.lastIndexOfIndicator(input) > 10){
-				return input.substring(10, this.lastIndexOfIndicator(input));
+			if(input.length() < this.lastIndexOfIndicator(input)){
+				return input.substring(0, this.lastIndexOfIndicator(input));
 			}else{
 				return "Its reminder";
 			}
